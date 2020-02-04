92a328d3bf6f4f78f8db64890390fb0545265349
seymonyu <59199817+seymonyu@users.noreply.github.com>
2020-02-04 15:15:27 +0100

    changed random person

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ function App() {
   return (
     <div className="App">
       <Contact
-        name="Seyma Fırat"
+        name="Seyma Firat"
         status="online"
         avatar="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
       />
       <Contact
         name="Joan Griffin"
         status="offline"
-        avatar="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
+        avatar="https://randomuser.me/api/portraits/men/66.jpg"
       />
       <Contact
-        name="Seyma Fırat"
+        name="Jessie Martinez"
         status="online"
-        avatar="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
+        avatar="https://randomuser.me/api/portraits/women/2.jpg"
       />
     </div>
   );
